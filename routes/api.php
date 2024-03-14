0319d31e8e1d4113acd7d42f136954684aa7ad85
Radoslav Berov <radoslavv.berov@gmail.com>
2024-03-14 20:13:06 +0200

    add get user endpoint

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::post('/change-password', [AuthController::class, 'changePassword']);
     Route::post('/auth/edit', [AuthController::class, 'editUser']);
+    Route::get('/auth/me', [AuthController::class, 'getCurrentUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::prefix('advertisements')->controller(AdvertisementController::class)->group(function() {
